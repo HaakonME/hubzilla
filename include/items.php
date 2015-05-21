62df06b3d6b2e56d271b69bebf8427caa715dd16
redmatrix <redmatrix@redmatrix.me>
2015-05-20 20:20:35 -0700

    cloning abook entries was broken in several ways if the target xchan wasn't available on the clone site. This should make it less broken.

--- include/items.php
+++ include/items.php
@@ -971,6 +971,14 @@ function import_author_diaspora($x) {
 	if(! $x['address'])
 		return false;
 
+	$r = q("select * from xchan where xchan_addr = '%s' limit 1",
+		dbesc($x['address'])
+	);
+	if($r) {
+		logger('in_cache: ' . $x['address'], LOGGER_DATA);
+		return $r[0]['chan_hash'];
+	}
+
 	if(discover_by_webbie($x['address'])) {
 		$r = q("select xchan_hash from xchan where xchan_addr = '%s' limit 1",
 			dbesc($x['address'])

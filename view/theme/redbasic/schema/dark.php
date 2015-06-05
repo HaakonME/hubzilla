f51cfa696052fb38123dd2f495b98fd2feb6d24c
jeroenpraat <jeroenpraat@xs4all.nl>
2015-06-05 21:15:21 +0200

    More schemas in new format

--- view/theme/redbasic/schema/dark.php
+++ view/theme/redbasic/schema/dark.php
@@ -16,42 +16,16 @@
 		$nav_icon_colour = "#999";
 	if (! $nav_active_icon_colour)
 		$nav_active_icon_colour = "#fff";
-	if (! $navtabs_borderc)
-		$navtabs_borderc = "#333";
-	if (! $navtabs_fontcolour)
-		$navtabs_fontcolour = "#fff";
-	if (! $navtabs_bgcolour)
-		$navtabs_bgcolour = "#111";
-	if (! $navtabs_linkcolour)
-		$navtabs_linkcolour = "color: #ccc;";
-	if (! $navtabs_linkchover)
-		$navtabs_linkchover = "color: #fff;";
-	if (! $navtabs_bgchover)
-		$navtabs_bgchover = "#222";
-	if (! $navtabs_decohover)
-		$navtabs_decohover = "underline";
-	if (! $navaside_bghover)
-		$navaside_bghover = "#222";
 	if (! $link_colour)
 		$link_colour = "#fff";
-	if (! $link_font_weight)
-		$link_font_weight = "bold";
-	if (! $selected_active_colour)
-		$selected_active_colour = "#fff";
-	if (! $selected_active_deco)
-		$selected_active_deco = "underline";
 	if (! $banner_colour)
 		$banner_colour = "#999";
-	if (! $search_background)
-		$search_background = "#999";
 	if (! $bgcolour)
 		$bgcolour = "#111";
 	if (! $item_colour)
 		$item_colour = "rgba(28,28,28,0.8)";
 	if (! $comment_item_colour)
 		$comment_item_colour = "rgba(18,18,18,0.4)";
-	if (! $genericcontent_bgcolour)
-		$genericcontent_bgcolour = "rgba(28,28,28,0.8)";
 	if (! $comment_border_colour)
 		$comment_border_colour = "rgba(28,28,28,0.8)";
 	if (! $toolicon_colour)
@@ -60,55 +34,5 @@
 		$toolicon_activecolour = '#fff';
 	if (! $font_colour)
 		$font_colour = "#ccc";
-	if (! $blockquote_colour)
-		$blockquote_colour = "#ccc";
-	if (! $blockquote_bgcolour)
-		$blockquote_bgcolour = "#1e1e1e";
-	if (! $blockquote_bordercolour)
-		$blockquote_bordercolour = "#666";
-	if (! $code_borderc)
-		$code_borderc = "#090909";
-	if (! $code_bgcolour)
-		$code_bgcolour = "#000";
-	if (! $code_txtcolour)
-		$code_txtcolour = "#fff";
-	if (! $pre_borderc)
-		$pre_borderc = "#090909";
-	if (! $pre_bgcolour)
-		$pre_bgcolour = "#000";
-	if (! $pre_txtcolour)
-		$pre_txtcolour = "#fff";
-	if (! $item_bordercolour)
-		$item_bordercolour = "#333";
-	if (! $notif_itemcolour)
-		$notif_itemcolour = "#ccc";
-	if (! $notif_itemhovercolour)
-		$notif_itemhovercolour = "#fff";
-	if (! $acpopup_bgcolour)
-		$acpopup_bgcolour = "#222";
-	if (! $acpopup_bordercolour)
-		$acpopup_bordercolour = "#333";
-	if (! $acpopup_tgbl_bgcolour)
-		$acpopup_tgbl_bgcolour = "#333";
-	if (! $acpopup_hovercolour)
-		$acpopup_hovercolour = "#fff";
-	if (! $editbuttons_bghover)
-		$editbuttons_bghover = "#222";
-	if (! $dropdown_bgcolour)
-		$dropdown_bgcolour = "#222";
-	if (! $dropdown_textcolour)
-		$dropdown_textcolour = "#ccc";
-	if (! $dropdown_txtcolhover)
-		$dropdown_txtcolhover = "#ddd";
-	if (! $dropdown_bgcolhover)
-		$dropdown_bgcolhover = "#333";
-	if (! $dropdown_bgimghover)
-		$dropdown_bgimghover = "background-image: none;";
-	if (! $dropdown_togglecol)
-		$dropdown_togglecol = "#ccc";
-	if (! $dropdown_togglebgcol)
-		$dropdown_togglebgcol = "#222";
-	if (! $dropdown_bordercol)
-		$dropdown_bordercol = "#222";
-	if (! $preview_backgroundimg)
-		$preview_backgroundimg = "gray_and_black_diagonal_stripes_background_seamless.gif";
+
+

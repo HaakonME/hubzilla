18798b1d12c1716b395d89bf214fdd42f41fd2d1
Mario Vavti <mario@mariovavti.com>
2015-07-07 12:23:19 +0200

    schema focus: adjust width to match photo width in /photos

--- view/theme/redbasic/schema/focus.php
+++ view/theme/redbasic/schema/focus.php
@@ -46,7 +46,7 @@ if (! $radius)
 if (! $shadow)
 	$shadow = "0";
 if (! $converse_width)
-	$converse_width = "676";
+	$converse_width = "656";
 if(! $top_photo)
 	$top_photo = '48px';
 if(! $comment_indent)

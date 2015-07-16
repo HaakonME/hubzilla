HaakonME/hubzilla

commit ec23a94b6d7f6db1e1d34275b8143cbc947db802

Make item_store_update only affect the data we requested

include/items.php
@@ -2548,8 +2548,8 @@ function item_store_update($arr,$allow_exec = false) {
 	$arr['deny_gid']      = ((array_key_exists('deny_gid',$arr))   ? trim($arr['deny_gid'])  : $orig[0]['deny_gid']);
 	$arr['item_private']  = ((array_key_exists('item_private',$arr)) ? intval($arr['item_private']) : $orig[0]['item_private']);
 
-	$arr['title'] = ((array_key_exists('title',$arr) && strlen($arr['title']))  ? trim($arr['title']) : '');
-	$arr['body']  = ((array_key_exists('body',$arr) && strlen($arr['body']))    ? trim($arr['body'])  : '');
+	$arr['title'] = ((array_key_exists('title',$arr))  ? trim($arr['title']) : $orig[0]['title']);
+	$arr['body']  = ((array_key_exists('body',$arr))   ? trim($arr['body'])  : $orig[0]['body']);
 
 	$arr['attach']        = ((x($arr,'attach'))        ? notags(trim($arr['attach']))        : $orig[0]['attach']);
 	$arr['app']           = ((x($arr,'app'))           ? notags(trim($arr['app']))           : $orig[0]['app']);
@@ -2599,12 +2599,11 @@ function item_store_update($arr,$allow_exec = false) {
 		return $ret;
 	}
 
-	$r = q("delete from term where oid = %d and otype = %d",
-		intval($orig_post_id),
-		intval(TERM_OBJ_POST)
-	);
-
-	if(($terms) && (is_array($terms))) {
+	if(is_array($terms)) {
+		$r = q("delete from term where oid = %d and otype = %d",
+			intval($orig_post_id),
+			intval(TERM_OBJ_POST)
+		);
 		foreach($terms as $t) {
 			q("insert into term (uid,oid,otype,type,term,url)
 				values(%d,%d,%d,%d,'%s','%s') ",
@@ -2630,6 +2629,8 @@ function item_store_update($arr,$allow_exec = false) {
 
 	return $ret;
 }
+
+
 
 function store_diaspora_comment_sig($datarray, $channel, $parent_item, $post_id, $walltowall = false) {
 

commit 39a09eb525df95a38c14bf3e6f9b8c74bda68314

fix missing div and a regression introduced in ec23a94b6d7f6db1e1d34275b8143cbc947db802 which broke tag removal for photos

include/items.php
@@ -2657,11 +2657,12 @@ function item_store_update($arr,$allow_exec = false) {
 		return $ret;
 	}
 
+	$r = q("delete from term where oid = %d and otype = %d",
+		intval($orig_post_id),
+		intval(TERM_OBJ_POST)
+	);
+
 	if(is_array($terms)) {
-		$r = q("delete from term where oid = %d and otype = %d",
-			intval($orig_post_id),
-			intval(TERM_OBJ_POST)
-		);
 		foreach($terms as $t) {
 			q("insert into term (uid,oid,otype,type,term,url)
 				values(%d,%d,%d,%d,'%s','%s') ",
@@ -2673,7 +2674,6 @@ function item_store_update($arr,$allow_exec = false) {
 				dbesc($t['url'])
 			);
 		}
-
 		$arr['term'] = $terms;
 	}
 

view/tpl/photo_view.tpl
@@ -2,7 +2,6 @@
 <div class="generic-content-wrapper">
 	<div class="section-title-wrapper">
 		<div class="pull-right">
-
 			{{if $tools}}
 			<a class="btn btn-default btn-xs" title="{{$tools.profile.1}}" href="{{$tools.profile.0}}"><i class="icon-user"></i></a>
 			{{/if}}
@@ -11,7 +10,6 @@
 				<i class="icon-globe btn btn-default btn-xs" title="{{$map_text}}" onclick="var pos = $('#photo-map').css('position'); if(pos === 'absolute') { $('#photo-map').css( { position: 'relative', left: 'auto', top: 'auto' }); } else { $('#photo-map').css( { position: 'absolute', left: '-9999px', top: '-9999px' }); }" ></i>
 			</div>
 			{{/if}}
-
 			<div class="btn-group btn-group dropdown">
 				{{if $edit}}
 				<i class="icon-pencil btn btn-default btn-xs" title="{{$edit.edit}}" onclick="openClose('photo-edit');"></i>
@@ -29,11 +27,8 @@
 				{{/if}}
 			</div>
 		</div>
-
 		<h2>{{if $desc}}{{$desc}}{{elseif $filename}}{{$filename}}{{else}}{{$unknown}}{{/if}}</h2>
-
 		<div class="clear"></div>
-
 	</div>
 	<div id="photo-map">
 	{{$map}}
@@ -88,12 +83,9 @@
 		</form>
 		<div id="photo-edit-end" class="clear"></div>
 	</div>
-
 	<div id="photo-view-wrapper">
-
 		<div id="photo-photo"><a href="{{$photo.href}}" title="{{$photo.title}}" onclick="$.colorbox({href: '{{$photo.href}}'}); return false;"><img style="width: 100%;" src="{{$photo.src}}"></a></div>
 		<div id="photo-photo-end" class="clear"></div>
-
 		{{if $tags}}
 		<div class="photo-item-tools-left" id="in-this-photo">
 			<span id="in-this-photo-text">{{$tag_hdr}}</span>
@@ -102,31 +94,29 @@
 			{{/foreach}}
 		</div>
 		{{/if}}
-
 		<div class="photo-item-tools">
-
-		{{if $responses.count }}
-		<div class="photo-item-tools-left pull-left">
-			<div class="{{if $responses.count > 1}}btn-group{{/if}}">
-			{{foreach $responses as $verb=>$response}}
-				{{if $response.count}}
-				<div class="btn-group">
-					<button type="button" class="btn btn-default btn-sm wall-item-like dropdown-toggle" data-toggle="dropdown" id="wall-item-{{$verb}}-{{$id}}">{{$response.count}} {{$response.button}}</button>
-					{{if $response.list_part}}
-					<ul class="dropdown-menu" role="menu" aria-labelledby="wall-item-{{$verb}}-{{$id}}">{{foreach $response.list_part as $liker}}<li role="presentation">{{$liker}}</li>{{/foreach}}</ul>
-					{{else}}
-					<ul class="dropdown-menu" role="menu" aria-labelledby="wall-item-{{$verb}}-{{$id}}">{{foreach $response.list as $liker}}<li role="presentation">{{$liker}}</li>{{/foreach}}</ul>
-					{{/if}}
-					{{if $response.list_part}}
+			{{if $responses.count }}
+			<div class="photo-item-tools-left pull-left">
+				<div class="{{if $responses.count > 1}}btn-group{{/if}}">
+				{{foreach $responses as $verb=>$response}}
+					{{if $response.count}}
+					<div class="btn-group">
+						<button type="button" class="btn btn-default btn-sm wall-item-like dropdown-toggle" data-toggle="dropdown" id="wall-item-{{$verb}}-{{$id}}">{{$response.count}} {{$response.button}}</button>
+						{{if $response.list_part}}
+						<ul class="dropdown-menu" role="menu" aria-labelledby="wall-item-{{$verb}}-{{$id}}">{{foreach $response.list_part as $liker}}<li role="presentation">{{$liker}}</li>{{/foreach}}</ul>
+						{{else}}
+						<ul class="dropdown-menu" role="menu" aria-labelledby="wall-item-{{$verb}}-{{$id}}">{{foreach $response.list as $liker}}<li role="presentation">{{$liker}}</li>{{/foreach}}</ul>
+						{{/if}}
+						{{if $response.list_part}}
 						<div class="modal" id="{{$verb}}Modal-{{$id}}">
 							<div class="modal-dialog">
 								<div class="modal-content">
 									<div class="modal-header">
 										<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-											<h4 class="modal-title">{{$response.title}}</h4>
+										<h4 class="modal-title">{{$response.title}}</h4>
 									</div>
 									<div class="modal-body">
-									<ul>{{foreach $response.list as $liker}}<li role="presentation">{{$liker}}</li>{{/foreach}}</ul>
+										<ul>{{foreach $response.list as $liker}}<li role="presentation">{{$liker}}</li>{{/foreach}}</ul>
 									</div>
 									<div class="modal-footer clear">
 										<button type="button" class="btn btn-default" data-dismiss="modal">{{$modal_dismiss}}</button>
@@ -134,37 +124,34 @@
 								</div><!-- /.modal-content -->
 							</div><!-- /.modal-dialog -->
 						</div><!-- /.modal -->
+						{{/if}}
+					</div>
 					{{/if}}
+				{{/foreach}}
 				</div>
-				{{/if}}
-			{{/foreach}}
+			</div>
+			{{/if}}
+			{{if $likebuttons}}
+			<div class="photo-item-tools-right btn-group pull-right">
+				<button type="button" class="btn btn-default btn-sm" onclick="dolike({{$id}},'like'); return false">
+					<i class="icon-thumbs-up-alt" title="{{$likethis}}"></i>
+				</button>
+				<button type="button" class="btn btn-default btn-sm" onclick="dolike({{$id}},'dislike'); return false">
+					<i class="icon-thumbs-down-alt" title="{{$nolike}}"></i>
+				</button>
+			</div>
+			<div id="like-rotator-{{$id}}" class="photo-like-rotator pull-right"></div>
+			{{/if}}
+			<div class="clear"></div>
 		</div>
-		{{/if}}
-		</div>
-		{{if $likebuttons}}
-		<div class="photo-item-tools-right btn-group pull-right">
-			<button type="button" class="btn btn-default btn-sm" onclick="dolike({{$id}},'like'); return false">
-				<i class="icon-thumbs-up-alt" title="{{$likethis}}"></i>
-			</button>
-			<button type="button" class="btn btn-default btn-sm" onclick="dolike({{$id}},'dislike'); return false">
-				<i class="icon-thumbs-down-alt" title="{{$nolike}}"></i>
-			</button>
-		</div>
-		<div id="like-rotator-{{$id}}" class="photo-like-rotator pull-right"></div>
-		{{/if}}
-		<div class="clear"></div>
 	</div>
-
 	{{$comments}}
-
 	{{if $commentbox}}
 	<div class="wall-item-comment-wrapper{{if $comments}} wall-item-comment-wrapper-wc{{/if}}" >
 		{{$commentbox}}
 	</div>
 	{{/if}}
-
 	<div class="clear"></div>
 </div>
-
 {{$paginate}}
 
f6142d18c43a77473dd4e2e3ea6c25154487ff2b
Mario Vavti <mario@mariovavti.com>
2015-07-26 20:54:09 +0200

    Provide connection status if status is "a-normal"

--- mod/connedit.php
+++ mod/connedit.php
@@ -520,6 +520,7 @@ function connedit_content(&$a) {
 				'url'   => $a->get_baseurl(true) . '/connedit/' . $contact['abook_id'] . '/block', 
 				'sel'   => (($contact['abook_flags'] & ABOOK_FLAG_BLOCKED) ? 'active' : ''),
 				'title' => t('Block (or Unblock) all communications with this connection'),
+				'info'   => (($contact['abook_flags'] & ABOOK_FLAG_BLOCKED) ? t('This connection is blocked!') : ''),
 			),
 
 			'ignore' => array(
@@ -527,6 +528,7 @@ function connedit_content(&$a) {
 				'url'   => $a->get_baseurl(true) . '/connedit/' . $contact['abook_id'] . '/ignore', 
 				'sel'   => (($contact['abook_flags'] & ABOOK_FLAG_IGNORED) ? 'active' : ''),
 				'title' => t('Ignore (or Unignore) all inbound communications from this connection'),
+				'info'   => (($contact['abook_flags'] & ABOOK_FLAG_IGNORED) ? t('This connection is ignored!') : ''),
 			),
 
 			'archive' => array(
@@ -534,6 +536,7 @@ function connedit_content(&$a) {
 				'url'   => $a->get_baseurl(true) . '/connedit/' . $contact['abook_id'] . '/archive', 
 				'sel'   => (($contact['abook_flags'] & ABOOK_FLAG_ARCHIVED) ? 'active' : ''),
 				'title' => t('Archive (or Unarchive) this connection - mark channel dead but keep content'),
+				'info'   => (($contact['abook_flags'] & ABOOK_FLAG_ARCHIVED) ? t('This connection is archived!') : ''),
 			),
 
 			'hide' => array(
@@ -541,6 +544,7 @@ function connedit_content(&$a) {
 				'url'   => $a->get_baseurl(true) . '/connedit/' . $contact['abook_id'] . '/hide', 
 				'sel'   => (($contact['abook_flags'] & ABOOK_FLAG_HIDDEN) ? 'active' : ''),
 				'title' => t('Hide or Unhide this connection from your other connections'),
+				'info'   => (($contact['abook_flags'] & ABOOK_FLAG_HIDDEN) ? t('This connection is hidden!') : ''),
 			),
 
 			'delete' => array(

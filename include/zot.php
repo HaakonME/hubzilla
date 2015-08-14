73b30de9cb22732f51ccca26695eced54683a895
Alexandre Hannud Abdo <abdo@member.fsf.org>
2015-08-14 12:21:05 -0300

    Properly fix which perms get checked for comments, Mike's solution

--- include/zot.php
+++ include/zot.php
@@ -1560,20 +1560,9 @@ function process_delivery($sender, $arr, $deliveries, $relay, $public = false, $
 
 		$tag_delivery = tgroup_check($channel['channel_id'],$arr);
 
-		$perm = (($arr['mid'] == $arr['parent_mid']) ? 'send_stream' : 'post_comments');
-
-//		if ($arr['mid'] == $arr['parent_mid']){
-//			$perm = 'send_stream';
-//		}
-//		else{
-//			$r = q("select item_owner from item where item.mid = '%s' limit 1",
-//				dbesc($arr['parent_mid']));
-//			if($channel['channel_hash'] == $r[0]['item_owner'])
-//				$perm = 'post_comments';
-//			else
-//				$perm = 'send_stream';
-//		}
-//
+		$perm = 'send_stream';
+		if(($arr['mid'] !== $arr['parent_mid']) && ($relay))
+			$perm = 'post_comments';
 
 		// This is our own post, possibly coming from a channel clone
 

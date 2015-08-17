HaakonME/hubzilla

commit 631eb4b2c213cf612e97f6a90ff1dbf1352eadad

issue #386 - account and channel bulk edit checkboxes not visible and therefore not useable. Also change wording to remove the offensive 'user' designation.

view/css/mod_admin.css
@@ -61,7 +61,12 @@
 #adminpage table tr:hover { background-color: #bbc7d7; }
 #adminpage .selectall { text-align: right; }
 
+.checkbox {
+   width: 1.2em;
+   height: 1.2em;
+}
 
 .channels_ckbx, .pending_ckbx, .users_ckbx {
-	margin-top: -5px !important;
+	margin-top: 0px !important;
+	margin-left: 0px !important;
 }

mod/admin.php
@@ -680,7 +680,7 @@ function admin_page_users_post($a) {
 				intval($users[$i])
 			);
 		}
-		notice( sprintf( tt("%s user blocked/unblocked", "%s users blocked/unblocked", count($users)), count($users)) );
+		notice( sprintf( tt("%s account blocked/unblocked", "%s account blocked/unblocked", count($users)), count($users)) );
 	}
 	// account delete button was submitted
 	if (x($_POST, 'page_users_delete')) {
@@ -688,7 +688,7 @@ function admin_page_users_post($a) {
 		foreach ($users as $uid){
 			account_remove($uid, true, false);
 		}
-		notice( sprintf( tt("%s user deleted", "%s users deleted", count($users)), count($users)) );
+		notice( sprintf( tt("%s account deleted", "%s accounts deleted", count($users)), count($users)) );
 	}
 	// registration approved button was submitted
 	if (x($_POST, 'page_users_approve')) {
@@ -736,7 +736,7 @@ function admin_page_users(&$a){
 				require_once('include/Contact.php');
 				account_remove($uid,true,false);
 
-				notice( sprintf(t("User '%s' deleted"), $account[0]['account_email']) . EOL);
+				notice( sprintf(t("Account '%s' deleted"), $account[0]['account_email']) . EOL);
 				break;
 			case 'block':
 				q("UPDATE account SET account_flags = ( account_flags | %d ) WHERE account_id = %d",
@@ -744,7 +744,7 @@ function admin_page_users(&$a){
 					intval($uid)
 				);
 
-				notice( sprintf( t("User '%s' blocked") , $account[0]['account_email']) . EOL);
+				notice( sprintf( t("Account '%s' blocked") , $account[0]['account_email']) . EOL);
 				break;
 			case 'unblock':
 				q("UPDATE account SET account_flags = ( account_flags & ~%d ) WHERE account_id = %d",
@@ -752,7 +752,7 @@ function admin_page_users(&$a){
 						intval($uid)
 				);
 
-				notice( sprintf( t("User '%s' unblocked"), $account[0]['account_email']) . EOL);
+				notice( sprintf( t("Account '%s' unblocked"), $account[0]['account_email']) . EOL);
 				break;
 		}
 
@@ -830,8 +830,8 @@ function admin_page_users(&$a){
 		'$h_users' => t('Users'),
 		'$th_users' => array( t('ID'), t('Email'), t('All Channels'), t('Register date'), t('Last login'), t('Expires'), t('Service Class')),
 
-		'$confirm_delete_multi' => t('Selected users will be deleted!\n\nEverything these users had posted on this site will be permanently deleted!\n\nAre you sure?'),
-		'$confirm_delete' => t('The user {0} will be deleted!\n\nEverything this user has posted on this site will be permanently deleted!\n\nAre you sure?'),
+		'$confirm_delete_multi' => t('Selected accounts will be deleted!\n\nEverything these accounts had posted on this site will be permanently deleted!\n\nAre you sure?'),
+		'$confirm_delete' => t('The account {0} will be deleted!\n\nEverything this account has posted on this site will be permanently deleted!\n\nAre you sure?'),
 
 		'$form_security_token' => get_form_security_token("admin_users"),
 

view/theme/redbasic/css/style.css
@@ -2293,6 +2293,6 @@ nav .badge.mail-update:hover {
     background-color: #337AB7;
 }
 
-.channels_ckbx, .pending_ckbx, .users_ckbx {
+/*.channels_ckbx, .pending_ckbx, .users_ckbx {
 	margin-top: -5px !important;
-}
+}*/

commit 98eeff7538f9580597845e689ff8ea9f63a7d601

commit 631eb4b broke the checkboxes in admin/site, so separate normal checkboxes from bulk edit checkboxes in the css

version.inc
@@ -1,1 +1,1 @@
-2015-08-16.1126
+2015-08-17.1127

view/css/mod_admin.css
@@ -61,7 +61,7 @@
 #adminpage table tr:hover { background-color: #bbc7d7; }
 #adminpage .selectall { text-align: right; }
 
-.checkbox {
+.checkbox_bulkedit {
    width: 1.2em;
    height: 1.2em;
 }

view/tpl/admin_channels.tpl
@@ -32,7 +32,7 @@
 						<td class='channel_id'>{{$c.channel_id}}</td>
 						<td class='channel_name'><a href="channel/{{$c.channel_address}}">{{$c.channel_name}}</a></td>
 						<td class='channel_address'>{{$c.channel_address}}</td>
-						<td class="checkbox"><input type="checkbox" class="channels_ckbx" id="id_channel_{{$c.channel_id}}" name="channel[]" value="{{$c.channel_id}}"/></td>
+						<td class="checkbox_bulkedit"><input type="checkbox" class="channels_ckbx" id="id_channel_{{$c.channel_id}}" name="channel[]" value="{{$c.channel_id}}"/></td>
 						<td class="tools">
 							<a href="{{$baseurl}}/admin/channels/block/{{$c.channel_id}}?t={{$form_security_token}}" class="btn btn-default btn-xs" title='{{if ($c.blocked)}}{{$unblock}}{{else}}{{$block}}{{/if}}'><i class='icon-ban-circle admin-icons {{if ($c.blocked)}}dim{{/if}}'></i></a>
 							<a href="{{$baseurl}}/admin/channels/code/{{$c.channel_id}}?t={{$form_security_token}}" class="btn btn-default btn-xs" title='{{if ($c.allowcode)}}{{$uncode}}{{else}}{{$code}}{{/if}}'><i class='icon-terminal admin-icons {{if ($c.allowcode)}}dim{{/if}}'></i></a>

view/tpl/admin_users.tpl
@@ -31,7 +31,7 @@
 				<tr>
 					<td class="created">{{$u.account_created}}</td>
 					<td class="email">{{$u.account_email}}</td>
-					<td class="checkbox"><input type="checkbox" class="pending_ckbx" id="id_pending_{{$u.hash}}" name="pending[]" value="{{$u.hash}}"></td>
+					<td class="checkbox_bulkedit"><input type="checkbox" class="pending_ckbx" id="id_pending_{{$u.hash}}" name="pending[]" value="{{$u.hash}}"></td>
 					<td class="tools">
 						<a href="{{$baseurl}}/regmod/allow/{{$u.hash}}" class="btn btn-default btn-xs" title="{{$approve}}"><i class="icon-thumbs-up-alt admin-icons"></i></a>
 						<a href="{{$baseurl}}/regmod/deny/{{$u.hash}}" class="btn btn-default btn-xs" title="{{$deny}}"><i class="icon-thumbs-down-alt admin-icons"></i></a>
@@ -71,7 +71,7 @@
 						<td class="login_date">{{$u.account_lastlog}}</td>
 						<td class="account_expires">{{$u.account_expires}}</td>
 						<td class="service_class">{{$u.account_service_class}}</td>
-						<td class="checkbox"><input type="checkbox" class="users_ckbx" id="id_user_{{$u.account_id}}" name="user[]" value="{{$u.account_id}}"><input type="hidden" name="blocked[]" value="{{$u.blocked}}"></td>
+						<td class="checkbox_bulkedit"><input type="checkbox" class="users_ckbx" id="id_user_{{$u.account_id}}" name="user[]" value="{{$u.account_id}}"><input type="hidden" name="blocked[]" value="{{$u.blocked}}"></td>
 						<td class="tools">
 							<a href="{{$baseurl}}/admin/users/{{if ($u.blocked)}}un{{/if}}block/{{$u.account_id}}?t={{$form_security_token}}"  class="btn btn-default btn-xs" title='{{if ($u.blocked)}}{{$unblock}}{{else}}{{$block}}{{/if}}'><i class="icon-ban-circle admin-icons{{if ($u.blocked)}} dim{{/if}}"></i></a><a href="{{$baseurl}}/admin/users/delete/{{$u.account_id}}?t={{$form_security_token}}" class="btn btn-default btn-xs" title='{{$delete}}' onclick="return confirm_delete('{{$u.name}}')"><i class="icon-trash admin-icons"></i></a>
 						</td>
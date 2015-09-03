669e1111187d1d1888358d833c62b5445a2b360b
redmatrix <redmatrix@redmatrix.me>
2015-09-02 21:07:24 -0700

    add timestamps for syncing apps

--- include/apps.php
+++ include/apps.php
@@ -342,7 +342,9 @@ function app_store($arr) {
 	$darray['app_page']     = ((x($arr,'page'))     ? escape_tags($arr['page']) : '');
 	$darray['app_requires'] = ((x($arr,'requires')) ? escape_tags($arr['requires']) : '');
 
-	$r = q("insert into app ( app_id, app_sig, app_author, app_name, app_desc, app_url, app_photo, app_version, app_channel, app_addr, app_price, app_page, app_requires ) values ( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, '%s', '%s', '%s', '%s' )",
+	$created = datetime_convert();
+
+	$r = q("insert into app ( app_id, app_sig, app_author, app_name, app_desc, app_url, app_photo, app_version, app_channel, app_addr, app_price, app_page, app_requires, app_created, app_edited ) values ( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, '%s', '%s', '%s', '%s', '%s', '%s' )",
 		dbesc($darray['app_id']),
 		dbesc($darray['app_sig']),
 		dbesc($darray['app_author']),
@@ -355,7 +357,9 @@ function app_store($arr) {
 		dbesc($darray['app_addr']),
 		dbesc($darray['app_price']),
 		dbesc($darray['app_page']),
-		dbesc($darray['app_requires'])
+		dbesc($darray['app_requires']),
+		dbesc($created),
+		dbesc($created)
 	);
 	if($r) {
 		$ret['success'] = true;
@@ -393,7 +397,9 @@ function app_update($arr) {
 	$darray['app_page']     = ((x($arr,'page')) ? escape_tags($arr['page']) : '');
 	$darray['app_requires'] = ((x($arr,'requires')) ? escape_tags($arr['requires']) : '');
 
-	$r = q("update app set app_sig = '%s', app_author = '%s', app_name = '%s', app_desc = '%s', app_url = '%s', app_photo = '%s', app_version = '%s', app_addr = '%s', app_price = '%s', app_page = '%s', app_requires = '%s' where app_id = '%s' and app_channel = %d",
+	$edited = datetime_convert();
+
+	$r = q("update app set app_sig = '%s', app_author = '%s', app_name = '%s', app_desc = '%s', app_url = '%s', app_photo = '%s', app_version = '%s', app_addr = '%s', app_price = '%s', app_page = '%s', app_requires = '%s', app_edited = '%s' where app_id = '%s' and app_channel = %d",
 		dbesc($darray['app_sig']),
 		dbesc($darray['app_author']),
 		dbesc($darray['app_name']),
@@ -405,6 +411,7 @@ function app_update($arr) {
 		dbesc($darray['app_price']),
 		dbesc($darray['app_page']),
 		dbesc($darray['app_requires']),
+		dbesc($edited),
 		dbesc($darray['app_id']),
 		intval($darray['app_channel'])
 	);
--- install/update.php
+++ install/update.php
@@ -1779,8 +1779,22 @@ function update_r1149() {
 
 function update_r1150() {
 
-	// hubzilla update does not apply here, but we're keeping the numbers in sync
-	return UPDATE_SUCCESS;
+	if(ACTIVE_DBTYPE == DBTYPE_POSTGRES) { 
+		$r1 = q("ALTER TABLE app ADD app_created timestamp NOT NULL DEFAULT '0001-01-01 00:00:00',
+			ADD app_edited timestamp NOT NULL DEFAULT '0001-01-01 00:00:00' ");
+	}
+	else {
+		$r1 = q("ALTER TABLE app ADD app_created DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+			ADD app_edited DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' ");
+	}
+
+	$r2 = q("create index app_created on app ( app_created ) ");
+	$r3 = q("create index app_edited on app ( app_edited ) ");
+
+	$r = $r1 && $r2 && $r3;
+    if($r)
+        return UPDATE_SUCCESS;
+    return UPDATE_FAILED;
 
 }
 

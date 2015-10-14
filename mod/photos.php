de697a42671e3c50106dab29eb108a122753a825
redmatrix <redmatrix@redmatrix.me>
2015-10-14 14:16:16 -0700

    issue #86 - like/dislike in photos not working

--- mod/photos.php
+++ mod/photos.php
@@ -982,13 +982,13 @@ function photos_content(&$a) {
 			$likebuttons = '';
 
 			if($can_post || $can_comment) {
-				$likebuttons = replace_macros($like_tpl,array(
-					'$id' => $link_item['id'],
-					'$likethis' => t("I like this \x28toggle\x29"),
-					'$nolike' => t("I don't like this \x28toggle\x29"),
-					'$share' => t('Share'),
-					'$wait' => t('Please wait')
-				));
+				$likebuttons = array(
+					'id' => $link_item['id'],
+					'likethis' => t("I like this \x28toggle\x29"),
+					'nolike' => t("I don't like this \x28toggle\x29"),
+					'share' => t('Share'),
+					'wait' => t('Please wait')
+				);
 			}
 
 			$comments = '';

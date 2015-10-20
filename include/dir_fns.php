08a5884c9f39c95200d9895cfc3739e77f07901a
redmatrix <redmatrix@redmatrix.me>
2015-10-19 17:03:34 -0700

    revert - we can't guarantee what project name the fallback directory server has and this could cause bugs later

--- include/dir_fns.php
+++ include/dir_fns.php
@@ -177,19 +177,17 @@ function sync_directories($dirmode) {
 			'site_update' => NULL_DATE, 
 			'site_directory' => DIRECTORY_FALLBACK_MASTER . '/dirsearch',
 			'site_realm' => DIRECTORY_REALM,
-			'site_valid' => 1,
-			'site_project' => PLATFORM_NAME
+			'site_valid' => 1
 			
 		);
-		$x = q("insert into site ( site_url, site_flags, site_update, site_directory, site_realm, site_valid, site_project )
-			values ( '%s', %d, '%s', '%s', '%s', %d, '%s' ) ",
+		$x = q("insert into site ( site_url, site_flags, site_update, site_directory, site_realm, site_valid )
+			values ( '%s', %d, '%s', '%s', '%s', %d ) ",
 			dbesc($r[0]['site_url']),
 			intval($r[0]['site_flags']),
 			dbesc($r[0]['site_update']),
 			dbesc($r[0]['site_directory']),
 			dbesc($r[0]['site_realm']),
-			intval($r[0]['site_valid']),
-			dbesc($r[0]['site_project'])
+			intval($r[0]['site_valid'])
 		);
 
 		$r = q("select * from site where (site_flags & %d) > 0 and site_url != '%s' and site_type = %d ",

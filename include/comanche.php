8d61efa0e865203df5eb4fae90b7bbd48597cf03
redmatrix <redmatrix@redmatrix.me>
2015-12-18 14:10:06 -0800

    allow widget 'packages' as well as individual files

--- include/comanche.php
+++ include/comanche.php
@@ -284,8 +284,13 @@ function comanche_widget($name, $text) {
 
 	$func = 'widget_' . trim($name);
 
-	if((! function_exists($func)) && file_exists('widget/' . trim($name) . '.php'))
-		require_once('widget/' . trim($name) . '.php');
+	if(! function_exists($func)) {
+		if(file_exists('widget/' . trim($name) . '.php'))
+			require_once('widget/' . trim($name) . '.php');
+		elseif(is_dir('widget/'. trim($name)) 
+			&& (file_exists('widget/' . trim($name) . '/' . trim($name) . '.php')))
+			require_once('widget/' . trim($name) . '/' . trim($name) . '.php');
+	}
 	else {
 		$theme_widget = $func . '.php';
 		if((! function_exists($func)) && theme_include($theme_widget))

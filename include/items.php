c107bcfbd972a900cfd81b217014f7d4915fd73d
redmatrix <git@macgirvin.com>
2016-02-18 00:51:36 -0800

    some necessary doco for set_iconfig()

--- include/items.php
+++ include/items.php
@@ -5504,6 +5504,22 @@ function get_iconfig(&$item, $family, $key) {
 
 }
 
+/**
+ * set_iconfig(&$item, $family, $key, $value, $sharing = false);
+ *
+ * $item - item array or item id. If passed an array the iconfig meta information is
+ *    added to the item structure (which will need to be saved with item_store eventually).
+ *    If passed an id, the DB is updated, but may not be federated and/or cloned.
+ * $family - namespace of meta variable
+ * $key - key of meta variable
+ * $value - value of meta variable
+ * $sharing - boolean (default false); if true the meta information is propagated with the item
+ *   to other sites/channels, mostly useful when $item is an array and has not yet been stored/delivered.
+ *   If the meta information is added after delivery and you wish it to be shared, it may be necessary to 
+ *   alter the item edited timestamp and invoke the delivery process on the updated item. The edited 
+ *   timestamp needs to be altered in order to trigger an item_store_update() at the receiving end.
+ */
+ 
 
 function set_iconfig(&$item, $family, $key, $value, $sharing = false) {
 

c97cc1487cbcac2c4fbc4d9e931a82df109af2e6
Mario Vavti <mario@mariovavti.com>
2016-03-02 12:31:25 +0100

    a simple cover-photo implementation for redbasic default template

--- view/theme/redbasic/js/redbasic.js
+++ view/theme/redbasic/js/redbasic.js
@@ -2,6 +2,17 @@
  * redbasic theme specific JavaScript
  */
 $(document).ready(function() {
+
+	//Simple cover-photo implementation
+	if($('#cover-photo').length && $(window).width() > 767) {
+		$('.navbar-fixed-top').css('position', 'relative');
+		$('main').css('padding-top', 0 + 'px');
+	}
+	else {
+		$('#cover-photo').remove();
+
+	}
+
 	// CSS3 calc() fallback (for unsupported browsers)
 	$('body').append('<div id="css3-calc" style="width: 10px; width: calc(10px + 10px); display: none;"></div>');
 	if( $('#css3-calc').width() == 10) {
@@ -38,9 +49,7 @@ $(document).ready(function() {
 	$("input[data-role=cat-tagsinput]").tagsinput({
 		tagClass: 'label label-primary'
 	});
-});
 
-$(document).ready(function(){
 	var doctitle = document.title;
 	function checkNotify() {
 		var notifyUpdateElem = document.getElementById('notify-update');
@@ -52,4 +61,14 @@ $(document).ready(function(){
 		}
 	}
 	setInterval(function () {checkNotify();}, 10 * 1000);
+});
+
+//Simple cover-photo implementation
+$(window).scroll(function () {
+	if($('#cover-photo').length && $(window).width() > 767 && $(window).scrollTop() >= $('#cover-photo').height()) {
+		$('main').css('padding-top', 71 + 'px');
+		$(window).scrollTop($(window).scrollTop() - $('#cover-photo').height())
+		$('.navbar-fixed-top').css('position', 'fixed');
+		$('#cover-photo').remove();
+	}
 });

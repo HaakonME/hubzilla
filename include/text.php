4c47d22f4b00a2536e681ef62225e3795acf378a
redmatrix <git@macgirvin.com>
2016-04-29 23:47:13 -0700

    trim engr_units string slight improvement to avoid subtle bugs

--- include/text.php
+++ include/text.php
@@ -1826,7 +1826,9 @@ function lang_selector() {
 
 
 function engr_units_to_bytes ($size_str) {
-	switch (substr ($size_str, -1)) {
+	if(! $size_str)
+		return $size_str;
+	switch (substr(trim($size_str), -1)) {
 		case 'M': case 'm': return (int)$size_str * 1048576;
 		case 'K': case 'k': return (int)$size_str * 1024;
 		case 'G': case 'g': return (int)$size_str * 1073741824;

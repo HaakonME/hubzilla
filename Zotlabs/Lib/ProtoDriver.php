bf050121506aa4cb487d9efd9cfdaa501ada3b1a
redmatrix <git@macgirvin.com>
2016-05-20 23:12:06 -0700

    relocate the (unused currently) protocol driver

--- Zotlabs/Lib/ProtoDriver.php
+++ Zotlabs/Lib/ProtoDriver.php
@@ -1,5 +1,7 @@
 <?php /** @file */
 
+namespace Zotlabs\Lib;
+
 /*
  * Abstraction class for dealing with alternate networks (which of course do not exist, hence the abstraction)
  */
@@ -13,31 +15,5 @@ abstract class ProtoDriver {
 	abstract protected function acknowledge_permissions($permissions,$channel,$recipient);
 	abstract protected function deliver_private($item,$channel,$recipients);
 	abstract protected function collect_private($channel,$connection);
-
-}
-
-class ZotDriver extends ProtoDriver {
-
-	protected function discover($channel,$location) {
-
-	}
-	protected function deliver($item,$channel,$recipients) {
-
-	}
-	protected function collect($channel,$connection) {
-
-	}
-	protected function change_permissions($permissions,$channel,$recipient) {
-
-	}
-	protected function acknowledge_permissions($permissions,$channel,$recipient) {
-
-	}
-	protected function deliver_private($item,$channel,$recipients) {
-
-	}
-	protected function collect_private($channel,$connection) {
-
-	}
 
 }

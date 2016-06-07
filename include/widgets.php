037cd74e8e02f048892208bedef0d16faef16af4
redmatrix <git@macgirvin.com>
2016-06-06 19:44:22 -0700

    rename admin/users to admin/accounts, provide sortable columns in account and channel summaries - issue #255

--- include/widgets.php
+++ include/widgets.php
@@ -1378,7 +1378,7 @@ function widget_admin($arr) {
 
 	$aside = array(
 		'site'      => array(z_root() . '/admin/site/',     t('Site'),           'site'),
-		'users'     => array(z_root() . '/admin/users/',    t('Accounts'),       'users', 'pending-update', t('Member registrations waiting for confirmation')),
+		'accounts'  => array(z_root() . '/admin/accounts/', t('Accounts'),       'accounts', 'pending-update', t('Member registrations waiting for confirmation')),
 		'channels'  => array(z_root() . '/admin/channels/', t('Channels'),       'channels'),
 		'security'  => array(z_root() . '/admin/security/', t('Security'),       'security'),
 		'features'  => array(z_root() . '/admin/features/', t('Features'),       'features'),

bc5f73e6c32cebd2add7a36173404208e7140af3
redmatrix <git@macgirvin.com>
2016-06-15 20:51:55 -0700

    provide a default for undefined languages

--- include/bbcode.php
+++ include/bbcode.php
@@ -495,6 +495,7 @@ function bb_highlight($match) {
 	if(in_array(strtolower($match[1]),['php','css','mysql','sql','abap','diff','html','perl','ruby',
 		'vbscript','avrc','dtd','java','xml','cpp','python','javascript','js','sh']))
 		return text_highlight($match[2],strtolower($match[1]));
+	return $match[0];
 }
 
 

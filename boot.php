953ca2c21ea9c1cc3390d153964a8979b466ed6d
redmatrix <git@macgirvin.com>
2016-06-19 19:15:26 -0700

    zot_revision should be string, not float

--- boot.php
+++ boot.php
@@ -46,7 +46,7 @@ require_once('include/account.php');
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
 define ( 'STD_VERSION',             '1.8' );
-define ( 'ZOT_REVISION',            1.1     );
+define ( 'ZOT_REVISION',            '1.1' );
 
 define ( 'DB_UPDATE_VERSION',       1176  );
 

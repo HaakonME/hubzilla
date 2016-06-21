63423c8ee1f750d855c75ed67205076d21eda4f1
redmatrix <git@macgirvin.com>
2016-06-20 20:56:58 -0700

    make sure the optimisations are reliable

--- include/text.php
+++ include/text.php
@@ -2050,7 +2050,7 @@ function ids_to_array($arr,$idx = 'id') {
 	$t = array();
 	if($arr) {
 		foreach($arr as $x) {
-			if(strlen($x[$idx]) && (! in_array($x[$idx],$t))) {
+			if(array_key_exists($idx,$x) && strlen($x[$idx]) && (! in_array($x[$idx],$t))) {
 				$t[] = $x[$idx];
 			}
 		}

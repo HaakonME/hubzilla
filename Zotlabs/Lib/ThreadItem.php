3704ff57cb65dce6ddd17136580e3bf82e7d97f2
redmatrix <git@macgirvin.com>
2016-06-27 16:50:06 -0700

    replace [+] and [-] with chevron icons

--- Zotlabs/Lib/ThreadItem.php
+++ Zotlabs/Lib/ThreadItem.php
@@ -418,7 +418,7 @@ class ThreadItem {
 			if(($nb_children > $visible_comments) || ($thread_level > 1)) {
 				$result['children'][0]['comment_firstcollapsed'] = true;
 				$result['children'][0]['num_comments'] = $comment_count_txt;
-				$result['children'][0]['hide_text'] = t('[+] show all');
+				$result['children'][0]['hide_text'] = sprintf( t('%s show all'), '<i class="fa fa-chevron-down"></i>');
 				if($thread_level > 1) {
 					$result['children'][$nb_children - 1]['comment_lastcollapsed'] = true;
 				}

339e620738ed90d18fdfb7e83b637b7d0e50f56e
redmatrix <git@macgirvin.com>
2016-06-29 21:03:53 -0700

    wiki featured disabled by default in UNO.

--- include/features.php
+++ include/features.php
@@ -52,7 +52,7 @@ function get_features($filtered = true) {
 			array('advanced_profiles',   t('Advanced Profiles'),      t('Additional profile sections and selections'),false,get_config('feature_lock','advanced_profiles')),
 			array('profile_export',      t('Profile Import/Export'),  t('Save and load profile details across sites/channels'),false,get_config('feature_lock','profile_export')),
 			array('webpages',            t('Web Pages'),              t('Provide managed web pages on your channel'),false,get_config('feature_lock','webpages')),
-			array('wiki',            t('Wiki'),              t('Provide a wiki for your channel'),true,get_config('feature_lock','wiki')),
+			array('wiki',            t('Wiki'),              t('Provide a wiki for your channel'),((UNO) ? false : true),get_config('feature_lock','wiki')),
 			array('hide_rating',       t('Hide Rating'),          t('Hide the rating buttons on your channel and profile pages. Note: People can still rate you somewhere else.'),false,get_config('feature_lock','hide_rating')),			
 			array('private_notes',       t('Private Notes'),          t('Enables a tool to store notes and reminders (note: not encrypted)'),false,get_config('feature_lock','private_notes')),
 			array('nav_channel_select',  t('Navigation Channel Select'), t('Change channels directly from within the navigation dropdown menu'),false,get_config('feature_lock','nav_channel_select')),

3beb94dc522453a0cfbf2705ed335b20aad5bc76
Mario Vavti <mario@mariovavti.com>
2016-07-18 13:52:26 +0200

    whitespace

--- boot.php
+++ boot.php
@@ -44,9 +44,7 @@ require_once('include/account.php');
 
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
-
 define ( 'STD_VERSION',             '1.11' );
-
 define ( 'ZOT_REVISION',            '1.1' );
 
 define ( 'DB_UPDATE_VERSION',       1180  );

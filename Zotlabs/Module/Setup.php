6b4cfe4f180a10e3008356c3a4590ad313fa88dd
redmatrix <mike@macgirvin.com>
2016-08-28 22:24:27 -0700

    Before throwing a 'smarty3 dir does not exist' fatal error, try and create it. We did try and create it during install, but the template processor is initalised long before we get to that code (and throws that ugly error).

--- Zotlabs/Module/Setup.php
+++ Zotlabs/Module/Setup.php
@@ -43,11 +43,11 @@ class Setup extends \Zotlabs\Web\Controller {
 			killme();
 		}
 	
-		if (x($_POST, 'pass'))
+		if (x($_POST, 'pass')) {
 			$this->install_wizard_pass = intval($_POST['pass']);
+		}
 		else {
 			$this->install_wizard_pass = 1;
-			@os_mkdir(TEMPLATE_BUILD_PATH, STORAGE_DEFAULT_PERMISSIONS, true);
 		}
 	}
 	

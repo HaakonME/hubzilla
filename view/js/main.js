68cb4baeb3213a2e6b24b4ad4526f1aaf6e741ec
Mario Vavti <mario@mariovavti.com>
2016-09-27 14:11:55 +0200

    do not scroll items behind the navbar

--- view/js/main.js
+++ view/js/main.js
@@ -653,7 +653,7 @@ function updateConvItems(mode,data) {
 
 	// auto-scroll to a particular comment in a thread (designated by mid) when in single-thread mode
 	if($('.item_' + bParam_mid.substring(0,32)).length && mode == 'replace') {
-		$('html, body').animate({ scrollTop: $('.item_' + bParam_mid.substring(0,32)).offset().top }, 'slow');
+		$('html, body').animate({ scrollTop: $('.item_' + bParam_mid.substring(0,32)).offset().top - $('nav').outerHeight(true) }, 'slow');
 		if($('.collapsed-comments').length) {
 			var scrolltoid = $('.collapsed-comments').attr('id').substring(19);
 			$('#collapsed-comments-' + scrolltoid).slideDown();

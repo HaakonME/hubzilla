5b6c2c32bff6c68ed5a35acf46f932aec6dad01d
redmatrix <mike@macgirvin.com>
2016-09-28 20:55:15 -0700

    Bring the saved search feature in line with the spec, and publish the feature spec.

--- Zotlabs/Module/Search_ac.php
+++ Zotlabs/Module/Search_ac.php
@@ -18,49 +18,68 @@ class Search_ac extends \Zotlabs\Web\Controller {
 			$search = $_REQUEST['query'];
 		}
 	
+		$do_people = true;
+		$do_tags = true;
+		
+		if(substr($search,0,1) === '@') {
+			$do_tags = false;
+			$search = substr($search,1);
+		}
+
+		if(substr($search,0,1) === '#') {
+			$do_people = false;
+			$search = substr($search,1);
+		}
+
 		// Priority to people searches
 	
 		if ($search) {
-			$people_sql_extra = protect_sprintf(" AND `xchan_name` LIKE '%". dbesc($search) . "%' ");
-			$tag_sql_extra = protect_sprintf(" AND term LIKE '%". dbesc($search) . "%' ");
+			$people_sql_extra = protect_sprintf(" AND xchan_name LIKE '%" . dbesc($search) . "%' ");
+			$tag_sql_extra = protect_sprintf(" AND term LIKE '%" . dbesc($search) . "%' ");
 		}
+
+		$results = [];
 	
+		if($do_people) {	
+			$r = q("SELECT abook_id, xchan_name, xchan_photo_s, xchan_url, xchan_addr FROM abook 
+				left join xchan on abook_xchan = xchan_hash WHERE abook_channel = %d 
+				$people_sql_extra
+				ORDER BY xchan_name ASC ",
+				intval(local_channel())
+			);
 	
-		$r = q("SELECT `abook_id`, `xchan_name`, `xchan_photo_s`, `xchan_url`, `xchan_addr` FROM `abook` left join xchan on abook_xchan = xchan_hash WHERE abook_channel = %d 
-			$people_sql_extra
-			ORDER BY `xchan_name` ASC ",
-			intval(local_channel())
-		);
-	
-		$results = array();
-		if($r) {
-			foreach($r as $g) {
-				$results[] = array(
-					"photo"    => $g['xchan_photo_s'],
-					"name"     => '@'.$g['xchan_name'],
-					"id"       => $g['abook_id'],
-					"link"     => $g['xchan_url'],
-					"label"    => '',
-					"nick"     => '',
-				);
+			if($r) {
+				foreach($r as $g) {
+					$results[] = [
+						'photo'    => $g['xchan_photo_s'],
+						'name'     => '@' . $g['xchan_name'],
+						'id'       => $g['abook_id'],
+						'link'     => $g['xchan_url'],
+						'label'    => '',
+						'nick'     => '',
+					];
+				}
 			}
 		}
+
+		if($do_tags) {	
+			$r = q("select distinct term, tid, url from term 
+				where ttype in ( %d, %d ) $tag_sql_extra group by term order by term asc",
+				intval(TERM_HASHTAG),
+				intval(TERM_COMMUNITYTAG)
+			);
 	
-		$r = q("select distinct term, tid, url from term where ttype in ( %d, %d ) $tag_sql_extra group by term order by term asc",
-			intval(TERM_HASHTAG),
-			intval(TERM_COMMUNITYTAG)
-		);
-	
-		if(count($r)) {
-			foreach($r as $g) {
-				$results[] = array(
-					"photo"    => z_root() . '/images/hashtag.png',
-					"name"     => '#'.$g['term'],
-					"id"       => $g['tid'],
-					"link"     => $g['url'],
-					"label"    => '',
-					"nick"     => '',
-				);
+			if($r) {
+				foreach($r as $g) {
+					$results[] = [
+						'photo'    => z_root() . '/images/hashtag.png',
+						'name'     => '#' . $g['term'],
+						'id'       => $g['tid'],
+						'link'     => $g['url'],
+						'label'    => '',
+						'nick'     => '',
+					];
+				}
 			}
 		}
 	
@@ -72,7 +91,7 @@ class Search_ac extends \Zotlabs\Web\Controller {
 		);
 		echo json_encode($o);
 	
-		logger('search_ac: ' . print_r($x,true),LOGGER_ALL,LOG_INFO);
+		logger('search_ac: ' . print_r($x,true),LOGGER_DATA,LOG_INFO);
 	
 		killme();
 	}

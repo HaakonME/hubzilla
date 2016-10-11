d5cd6f9a5137f53b8a50c81bbd4f4838c0707db1
Klaus Weidenbach <Klaus.Weidenbach@gmx.net>
2016-10-11 11:48:16 +0200

    Comment and code about UTF-8 @-sign do not match.
    Changed code according to comment:
    First AT is now full-width AT U+FF20.
    Do we also need to replace small AT U+FE6B?

--- include/attach.php
+++ include/attach.php
@@ -1948,9 +1948,9 @@ function get_attach_binname($s) {
 
 
 function get_dirpath_by_cloudpath($channel, $path) {
-	
-	// Warning: Do not edit the following line. The first symbol is UTF-8 &#65312; 
-	$path = str_replace('@','@',notags(trim($path)));		
+
+	// Warning: Do not edit the following line. The first symbol is UTF-8 (U+FF20) &#65312;
+	$path = str_replace('＠', '@', notags(trim($path)));
 
 	$h = @parse_url($path);
 

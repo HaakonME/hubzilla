c9aca2a13d024e7ac45e6cd51858d90f41bbc364
Mario Vavti <mario@mariovavti.com>
2016-12-12 14:42:08 +0100

    bump version

--- boot.php
+++ boot.php
@@ -48,7 +48,7 @@ require_once('include/zid.php');
 
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
-define ( 'STD_VERSION',             '1.15.2' );
+define ( 'STD_VERSION',             '2.1' );
 define ( 'ZOT_REVISION',            '1.2' );
 
 define ( 'DB_UPDATE_VERSION',       1185  );

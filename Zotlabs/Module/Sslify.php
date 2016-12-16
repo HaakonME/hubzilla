1d63654662c167c46b78efd451d4345929ae9e40
zotlabs <mike@macgirvin.com>
2016-12-15 18:06:49 -0800

    there are occasional sites which use different capitalisation of Content-Type; which is rare, but completely legal.

--- Zotlabs/Module/Sslify.php
+++ Zotlabs/Module/Sslify.php
@@ -10,21 +10,16 @@ class Sslify extends \Zotlabs\Web\Controller {
 			$h = explode("\n",$x['header']);
 			foreach ($h as $l) {
 				list($k,$v) = array_map("trim", explode(":", trim($l), 2));
-				$hdrs[$k] = $v;
+				$hdrs[strtolower($k)] = $v;
 			}
-			if (array_key_exists('Content-Type', $hdrs))
-				$type = $hdrs['Content-Type'];
-		
-			header('Content-Type: ' . $type);
+			if (array_key_exists('content-type', $hdrs)) {
+				$type = $hdrs['content-type'];	
+				header('Content-Type: ' . $type);
+			}
+
 			echo $x['body'];
 			killme();
 		}
 		killme();
-		// for some reason when this fallback is in place - it gets triggered
-		// often, (creating mixed content exceptions) even though there is 
-		// nothing obvious missing on the page when we bypass it. 
-		goaway($_REQUEST['url']);
-	}
-	
-	
+	}	
 }

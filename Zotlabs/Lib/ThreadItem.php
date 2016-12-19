92f3d863fa4a8a96d4f882a91a81d888a58230e5
zotlabs <mike@macgirvin.com>
2016-12-18 16:26:00 -0800

    re-factor item_photo_menu - there is some room for future development, currently it was made compatible with minimal template changes

--- Zotlabs/Lib/ThreadItem.php
+++ Zotlabs/Lib/ThreadItem.php
@@ -335,6 +335,8 @@ class ThreadItem {
 			'wall' => t('Wall-to-Wall'),
 			'vwall' => t('via Wall-To-Wall:'),
 			'profile_url' => $profile_link,
+			'thread_action_menu' => thread_action_menu($item,$conv->get_mode()),
+			'thread_author_menu' => thread_author_menu($item,$conv->get_mode()),
 			'item_photo_menu' => item_photo_menu($item),
 			'dreport' => $dreport,
 			'name' => $profile_name,

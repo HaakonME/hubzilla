8ea0b2051a2f037ee67571c01b816b3a286fb5ee
Mario Vavti <mario@mariovavti.com>
2017-01-13 13:04:30 +0100

    another try on preventing double scrollbars when a modal is larger than the viewport

--- view/theme/redbasic/js/redbasic.js
+++ view/theme/redbasic/js/redbasic.js
@@ -99,11 +99,13 @@ function makeFullScreen(full) {
 function toggleAside() {
 	$('#expand-aside-icon').toggleClass('fa-arrow-circle-right').toggleClass('fa-arrow-circle-left');
 	if($('main').hasClass('region_1-on')){
+		$('html, body').css('overflow-x', '');
 		$('main').removeClass('region_1-on')
 		$('#overlay').remove();
 		$('#left_aside_wrapper').trigger("sticky_kit:detach");
 	}
 	else {
+		$('html, body').css('overflow-x', 'hidden');
 		$('main').addClass('region_1-on')
 		$('<div id="overlay"></div>').appendTo('section');
 		$('#left_aside_wrapper').stick_in_parent({

a153368814b9753e06ab4731fd2b19a1c394b1b1
zotlabs <mike@macgirvin.com>
2017-03-13 18:58:23 -0700

    make redbasic compatible with red 5.x

--- view/theme/redbasic/php/theme.php
+++ view/theme/redbasic/php/theme.php
@@ -5,7 +5,7 @@
  *   * Description: Hubzilla standard theme
  *   * Version: 2.0
  *   * MinVersion: 2.2RC
- *   * MaxVersion: 3.0
+ *   * MaxVersion: 6.0
  *   * Author: Fabrixxm
  *   * Maintainer: Mike Macgirvin
  *   * Maintainer: Mario Vavti

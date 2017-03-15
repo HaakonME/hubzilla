df6c07aaadda2e89d2843bae136f813c909637ba
zotlabs <mike@macgirvin.com>
2017-03-15 16:55:47 -0700

    Comanche: allow widgets to be class based and stored appropriately in Zotlabs

--- include/widgets.php
+++ include/widgets.php
@@ -174,22 +174,6 @@ function widget_follow($args) {
 }
 
 
-function widget_notes($arr) {
-	if(! local_channel())
-		return '';
-	if(! feature_enabled(local_channel(),'private_notes'))
-		return '';
-
-	$text = get_pconfig(local_channel(),'notes','text');
-
-	$o = replace_macros(get_markup_template('notes.tpl'), array(
-		'$banner' => t('Notes'),
-		'$text' => $text,
-		'$save' => t('Save'),
-	));
-
-	return $o;
-}
 
 
 function widget_savedsearch($arr) {

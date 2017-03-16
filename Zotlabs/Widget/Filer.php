92d7c32f61e8d8a72141629e73bb3bba38546278
zotlabs <mike@macgirvin.com>
2017-03-15 19:30:57 -0700

    more widgets

---
+++ Zotlabs/Widget/Filer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Zotlabs\Widget;
+
+
+class Filer {
+
+	function widget($arr) {
+		if(! local_channel())
+			return '';
+
+
+		$selected = ((x($_REQUEST,'file')) ? $_REQUEST['file'] : '');
+
+		$terms = array();
+		$r = q("select distinct term from term where uid = %d and ttype = %d order by term asc",
+			intval(local_channel()),
+			intval(TERM_FILE)
+		);
+		if(! $r)
+			return;
+
+		foreach($r as $rr)
+			$terms[] = array('name' => $rr['term'], 'selected' => (($selected == $rr['term']) ? 'selected' : ''));
+
+		return replace_macros(get_markup_template('fileas_widget.tpl'),array(
+			'$title' => t('Saved Folders'),
+			'$desc' => '',
+			'$sel_all' => (($selected == '') ? 'selected' : ''),
+			'$all' => t('Everything'),
+			'$terms' => $terms,
+			'$base' => z_root() . '/' . \App::$cmd
+		));
+	}
+}

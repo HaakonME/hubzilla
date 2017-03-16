f60309efa12be3b8d4f5b2c565ebdf7f051a987d
zotlabs <mike@macgirvin.com>
2017-03-15 20:11:28 -0700

    more widget migrations

---
+++ Zotlabs/Widget/Categories.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Zotlabs\Widget;
+
+
+class Categories {
+
+	function widget($arr) {
+
+		if((! \App::$profile['profile_uid']) 
+			|| (! perm_is_allowed(\App::$profile['profile_uid'],get_observer_hash(),'view_stream'))) {
+			return '';
+		}
+
+		$cat = ((x($_REQUEST,'cat')) ? htmlspecialchars($_REQUEST['cat'],ENT_COMPAT,'UTF-8') : '');
+		$srchurl = \App::$query_string;
+		$srchurl =  rtrim(preg_replace('/cat\=[^\&].*?(\&|$)/is','',$srchurl),'&');
+		$srchurl = str_replace(array('?f=','&f='),array('',''),$srchurl);
+
+		return categories_widget($srchurl, $cat);
+	}
+}

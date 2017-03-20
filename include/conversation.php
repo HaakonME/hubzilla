cfdf1def2a24a7796ef86ab84d30fdbe734f3119
Mario Vavti <mario@mariovavti.com>
2017-03-20 14:09:27 +0100

    since we have the app bin we do not actually need the profile tabs for ourself

--- include/conversation.php
+++ include/conversation.php
@@ -1784,6 +1784,9 @@ function profile_tabs($a, $is_owner = false, $nickname = null){
 	$uid = ((App::$profile['profile_uid']) ? App::$profile['profile_uid'] : local_channel());
 	$account_id = ((App::$profile['profile_uid']) ? App::$profile['channel_account_id'] : App::$channel['channel_account_id']);
 
+	if ($uid == local_channel())
+		return;
+
 	if($uid == local_channel()) {
 		$cal_link = '';
 	}

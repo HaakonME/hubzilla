e98938d03dbdd94c33419924a50e4b6fbeb6fe1c
zotlabs <mike@macgirvin.com>
2017-03-23 00:20:12 -0700

    rev update

--- boot.php
+++ boot.php
@@ -49,7 +49,7 @@ require_once('include/hubloc.php');
 
 
 define ( 'PLATFORM_NAME',           'red' );
-define ( 'STD_VERSION',             '5.2' );
+define ( 'STD_VERSION',             '5.3' );
 define ( 'ZOT_REVISION',            '1.2' );
 
 define ( 'DB_UPDATE_VERSION',       1188  );

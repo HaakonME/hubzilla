954176cfc3e5c49e9ed74749bd52d2c8ff3c3852
Mario Vavti <mario@mariovavti.com>
2017-03-29 14:22:37 +0200

    bump version

--- boot.php
+++ boot.php
@@ -49,7 +49,7 @@ require_once('include/hubloc.php');
 
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
-define ( 'STD_VERSION',             '2.3.1' );
+define ( 'STD_VERSION',             '2.3.2' );
 define ( 'ZOT_REVISION',            '1.2' );
 
 define ( 'DB_UPDATE_VERSION',       1188  );

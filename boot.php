3b6a0d8e5cb787bace27aee67f7ae0f4130e3132
Mario Vavti <mario@mariovavti.com>
2017-03-29 15:55:21 +0200

    remove hubzilla.site from directory fallback servers until further notice

--- boot.php
+++ boot.php
@@ -81,7 +81,7 @@ define ( 'DIRECTORY_REALM',            'RED_GLOBAL');
 define ( 'DIRECTORY_FALLBACK_MASTER',  'https://gravizot.de');
 
 $DIRECTORY_FALLBACK_SERVERS = array( 
-	'https://hubzilla.site',
+	//'https://hubzilla.site',
 	'https://hubzilla.zottel.net',
 	'https://my.federated.social',
 	'https://hubzilla.nl',

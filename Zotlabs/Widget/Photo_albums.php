26125bcf0b8ce98fa4522f89792d7e77f25ff1ca
zotlabs <mike@macgirvin.com>
2017-04-02 22:20:37 -0700

    sql error photos_albums_list with non-logged-in viewer

--- Zotlabs/Widget/Photo_albums.php
+++ Zotlabs/Widget/Photo_albums.php
@@ -16,7 +16,7 @@ class Photo_albums {
 		if((! $channelx) || (! perm_is_allowed(\App::$profile['profile_uid'], get_observer_hash(), 'view_storage')))
 			return '';
 
-		$sortkey = ((array_key_exists('sortkey',$arr)) ? $arr['sortkey'] : 'album');
+		$sortkey = ((array_key_exists('sortkey',$arr)) ? $arr['sortkey'] : 'display_path');
 		$direction = ((array_key_exists('direction',$arr)) ? $arr['direction'] : 'asc');
 
 		return photos_album_widget($channelx, \App::get_observer(),$sortkey,$direction);

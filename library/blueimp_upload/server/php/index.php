a8374b25f15c1c7545748bae26dda5e3c190573c
zotlabs <mike@macgirvin.com>
2017-06-26 17:32:38 -0700

    upgrade blueimp from 9.8 to 9.18

--- library/blueimp_upload/server/php/index.php
+++ library/blueimp_upload/server/php/index.php
@@ -1,13 +1,13 @@
 <?php
 /*
- * jQuery File Upload Plugin PHP Example 5.14
+ * jQuery File Upload Plugin PHP Example
  * https://github.com/blueimp/jQuery-File-Upload
  *
  * Copyright 2010, Sebastian Tschan
  * https://blueimp.net
  *
  * Licensed under the MIT license:
- * http://www.opensource.org/licenses/MIT
+ * https://opensource.org/licenses/MIT
  */
 
 error_reporting(E_ALL | E_STRICT);

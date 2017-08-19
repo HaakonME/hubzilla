944cd883bbdb8d87d941238eef63d7f15922f2ab
zotlabs <mike@macgirvin.com>
2017-08-19 01:08:24 -0700

    script html tags in js file

--- view/js/mod_display.js
+++ view/js/mod_display.js
@@ -1,7 +1,5 @@
-<script>
 $(document).ready(function() {
 	$(".comment-edit-wrapper textarea").editor_autocomplete(baseurl+"/acl?f=&n=1");
 	// make auto-complete work in more places
 	$(".wall-item-comment-wrapper textarea").editor_autocomplete(baseurl+"/acl?f=&n=1");
 });
-</script>

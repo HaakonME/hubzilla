b80f0b896053f300d1c888719a93ce67c0542ce5
zotlabs <mike@macgirvin.com>
2017-08-28 20:11:08 -0700

    add editor autocompleters on cards page

--- view/js/mod_cards.js
+++ view/js/mod_cards.js
@@ -1,3 +1,9 @@
 $(document).ready( function() {
 	$(".autotime").timeago();
+
+	/* autocomplete @nicknames */
+	$(".comment-edit-form  textarea").editor_autocomplete(baseurl+"/acl?f=&n=1");
+	/* autocomplete bbcode */
+	$(".comment-edit-form  textarea").bbco_autocomplete('bbcode');
+
 });

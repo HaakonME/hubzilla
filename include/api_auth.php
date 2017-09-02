7bff60edacd68ef3dccf6f956e9c57092919950a
zotlabs <mike@macgirvin.com>
2017-09-02 14:04:37 -0700

    may be exploitable in current form - awaiting review

--- include/api_auth.php
+++ include/api_auth.php
@@ -85,7 +85,8 @@ function api_login(&$a){
 					else {
 						continue;
 					}
-
+// requires security review
+$record = null;
 					if($record) {					
 						$verified = \Zotlabs\Web\HTTPSig::verify('',$record['channel']['channel_pubkey']);
 						if(! ($verified && $verified['header_signed'] && $verified['header_valid'])) {

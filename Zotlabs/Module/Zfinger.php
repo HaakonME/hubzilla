47ef41695a3d30ca59ffb137745a7d3151b0df81
zotlabs <mike@macgirvin.com>
2017-09-03 19:56:13 -0700

    sign zot-info packets with httpsignatures

--- Zotlabs/Module/Zfinger.php
+++ Zotlabs/Module/Zfinger.php
@@ -9,8 +9,35 @@ class Zfinger extends \Zotlabs\Web\Controller {
 		require_once('include/zot.php');
 		require_once('include/crypto.php');
 	
-	
 		$x = zotinfo($_REQUEST);
+
+		if($x && $x['guid'] && $x['guid_sig']) {
+			$chan_hash = make_xchan_hash($x['guid'],$x['guid_sig']);
+			if($chan_hash) {
+				$chan = channelx_by_hash($chan_hash);
+			}
+		}
+
+		$headers = [];
+		$headers['Content-Type'] = 'application/json' ;
+		$ret = json_encode($x);
+
+		if($chan) {
+			$hash = \Zotlabs\Web\HTTPSig::generate_digest($ret,false);
+			$headers['Digest'] = 'SHA-256=' . $hash;  
+			\Zotlabs\Web\HTTPSig::create_sig('',$headers,$chan['channel_prvkey'],z_root() . '/channel/' . $chan['channel_address'],true);
+		}
+		else {
+			foreach($headers as $k => $v) {
+				header($k . ': ' . $v);
+			}
+		}
+
+		echo $ret;
+		killme();
+
+
+
 		json_return_and_die($x);
 	
 	}

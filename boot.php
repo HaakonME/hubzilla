5aa0fe2bf0a4f0b4b6e2fd0341448a61dc4b9044
Mario Vavti <mario@mariovavti.com>
2017-09-14 10:33:16 +0200

    use the hubzilla icons

--- boot.php
+++ boot.php
@@ -608,11 +608,11 @@ function sys_boot() {
 
 
 	if(! defined('DEFAULT_PLATFORM_ICON')) {
-		define( 'DEFAULT_PLATFORM_ICON', '/images/rm-32.png' );
+		define( 'DEFAULT_PLATFORM_ICON', '/images/hz-32.png' );
 	}
 
 	if(! defined('DEFAULT_NOTIFY_ICON')) {
-		define( 'DEFAULT_NOTIFY_ICON', '/images/rm-32.png' );
+		define( 'DEFAULT_NOTIFY_ICON', '/images/hz-white-64.png' );
 	}
 
 	App::head_set_icon(DEFAULT_PLATFORM_ICON);
